47fe8fa5735d90a6ccf822c2d7d50e83945ab957
狼龙凌刃 <61902032+lingren8847@users.noreply.github.com>
2022-12-06 16:19:34 +0800

    Update judge.lua

--- judge.lua
+++ judge.lua
@@ -1,5 +1,5 @@
 【脚本开关】开启【脚本开关】
-【测试服】 114514 【测试服】
+【测试服】https://raw.githubusercontent.com/lingren8847/lingren-Seasonal-Graduation-Ceremony-Tools/main/Judgment_of_international_service_version.lua【测试服】
 【国际服】  【国际服】
 【脚本公告】
 1.已适配最新版国际服0.19.5（206971）。
